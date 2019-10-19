31f260d0ac602770d6386d77a6d5177ca402a866
Manuel Reimer <manuel.reimer@gmx.de>
2019-10-19 10:40:57 +0200

    License header for every source file

--- options.js
+++ options.js
@@ -1,3 +1,21 @@
+/*
+    Firefox addon "RocWeb Extras"
+    Copyright (C) 2019  Manuel Reimer <manuel.reimer@gmx.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const input_new_url = document.getElementById("input_new_url");
 const button_new_url = document.getElementById("button_new_url");
 const button_delete = document.getElementById("button_delete");
